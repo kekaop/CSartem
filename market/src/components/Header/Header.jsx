fcdad49c4ffa72e1cb4ef0726fb4e542b97bd6cf
kekaop <mr.energee@gmail.com>
2025-04-09 10:19:07 +0700

    Первый коммит из Zed

--- market/src/components/Header/Header.jsx
+++ market/src/components/Header/Header.jsx
@@ -1,20 +1,19 @@
-import "./Header.css"
-import HeaderLink from "./HeaderLink/HeaderLink"
+import "./Header.css";
+import HeaderLink from "./HeaderLink/HeaderLink";
 
 export default function Header() {
-
   const datas = [
     {
       text: "На основной сайт",
       link: "https://symmetry-cs2.ru",
-      icon: "arrow_back"
-    }
-  ]
+      icon: "arrow_back",
+    },
+  ];
 
   return (
     <header>
       <div className="logo">
-        <img src="/logo.ico" width='36px' height='36px' />
+        <img src="/logo.ico" width="36px" height="36px" />
         <span>SYMMETRY CS2</span>
       </div>
       <ul className="header-items">
@@ -22,9 +21,8 @@ export default function Header() {
           <HeaderLink key={data.text} {...data} />
         ))}
       </ul>
-      <div className="auth-items">
-
-      </div>
+      <div className="auth-items"></div>
     </header>
-  )
+  );
 }
+//SSFSEFSEF
